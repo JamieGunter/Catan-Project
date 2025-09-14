465d26920eb9acf72ab5803af184f3e63d03106b
Jamie Gunter <JamieGunter@users.noreply.github.com>
2025-09-14 18:15:13 +0100

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: { origin: '*' } // Allows frontend to connect
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
